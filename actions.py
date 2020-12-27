3ec7894c0ad099656a1dcae53eab2b921ffead7d
Reda Elmarhouch <r.el.mar817991@gmail.com>
2020-12-27 13:22:54 +0000

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,18 +1,7 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List, Union
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
-
 
 class ActionFormInfo(FormAction):
     def name(self) -> Text:
@@ -34,8 +23,7 @@ class ActionFormInfo(FormAction):
     ) -> List[Dict]:
         """Define what the form has to do
             after all required slots are filled"""
-
-        # utter submit template
+        
         dispatcher.utter_message(template="utter_submit", name=tracker.get_slot('name'),
                                  puissance=tracker.get_slot('puissance'),
                                  carburant=tracker.get_slot('carburant'),
